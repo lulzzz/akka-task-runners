bf2ba08d73564a939c0edd65b2e2d86db39a7c51
tintoy <tintoy@tintoy.io>
2015-09-16 14:53:49 +1000

    Create initial task host app.

---
+++ TaskHost/Program.cs
@@ -0,0 +1,81 @@
+﻿using Akka;
+using Akka.Actor;
+using Akka.Configuration;
+using Serilog;
+using System;
+using System.Threading;
+
+namespace TaskHost
+{
+	/// <summary>
+	///		The host for the task-runner actors.
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		///		The main program entry-point.
+		/// </summary>
+		static void Main()
+		{
+			ConfigureLogging();
+
+			SynchronizationContext.SetSynchronizationContext(
+				new SynchronizationContext()
+			);
+
+            try
+			{
+				ActorTest();
+			}
+			catch (AggregateException eUnexpected)
+			{
+				eUnexpected.Flatten().Handle(
+					exception =>
+					{
+						Log.Error(
+							exception,
+							"Unexpected error: {ErrorMessage}",
+							exception.Message
+						);
+
+						return true;
+					}
+				);
+			}
+			catch (Exception eUnexpected)
+			{
+				Log.Error(
+					eUnexpected,
+					"Unexpected error: {ErrorMessage}",
+					eUnexpected.Message
+				);
+			}
+		}
+
+		/// <summary>
+		///		Quick test of actor usage.
+		/// </summary>
+		public static void ActorTest()
+		{
+			using (ActorSystem actorSystem = ActorSystem.Create("TaskRunner"))
+			{
+				// TODO: Define actor types.
+			}
+		}
+
+		/// <summary>
+		///		Enable the global logger.
+		/// </summary>
+		static void ConfigureLogging()
+		{
+			Log.Logger =
+				new LoggerConfiguration()
+					.MinimumLevel.Information()
+					.WriteTo.LiterateConsole()
+					.Enrich.WithMachineName()
+					.Enrich.WithProcessId()
+					.Enrich.WithThreadId()
+					.CreateLogger();
+		}
+	}
+}
